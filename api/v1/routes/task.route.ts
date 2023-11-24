41c1a5ace4387a4d47ba177f7b530a01f46078c2
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-11-24 23:08:51 +0700

    9.1. Tạo routes

---
+++ api/v1/routes/task.route.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from "express";
+import Task from "../../../models/task.model";
+
+const router: Router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false
+  });
+
+  res.json(tasks);
+});
+
+router.get("/detail/:id", async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+
+  const task = await Task.findOne({
+    _id: id,
+    deleted: false
+  });
+
+  res.json(task);
+});
+
+export const taskRoutes: Router = router;
