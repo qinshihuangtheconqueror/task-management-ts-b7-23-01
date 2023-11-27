e623f1c286ce340362f07a2485e8887166b67a59
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-11-27 23:01:31 +0700

    9. Cài đặt CORS

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/routes/index.route";
 
@@ -10,6 +11,14 @@ database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
+
+// const corsOptions = {
+//   origin: 'http://example.com'
+// };
+
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 // parse application/json
 app.use(bodyParser.json());
