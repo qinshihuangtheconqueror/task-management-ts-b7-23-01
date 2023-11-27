532624ee8100a8650e115d4e209b20d75e7caad7
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-11-27 21:59:38 +0700

    1. Phân trang

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,42 +1,62 @@
 import { Request, Response } from "express";
 import Task from "../models/task.model";
+import { paginationHelper } from "../../../helpers/pagination";
 
 export const index = async (req: Request, res: Response) => {
   // Find
   interface Find {
-    deleted: boolean,
-    status?: string
+    deleted: boolean;
+    status?: string;
   }
 
   const find: Find = {
-    deleted: false
+    deleted: false,
   };
 
-  if(req.query.status) {
+  if (req.query.status) {
     find.status = req.query.status.toString();
   }
   // End Find
 
+  // Pagination
+  const countTasks = await Task.countDocuments(find);
+
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 2,
+    },
+    req.query,
+    countTasks
+  );
+  // End Pagination
+
   // Sort
   const sort = {};
-  if(req.query.sortKey && req.query.sortValue) {
+  if (req.query.sortKey && req.query.sortValue) {
     const sortKey = req.query.sortKey.toString();
     sort[sortKey] = req.query.sortValue;
   }
   // End Sort
-  
-  const tasks = await Task.find(find).sort(sort);
 
-  res.json(tasks);
-}
+  const tasks = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
+
+  res.json({
+    total: countTasks,
+    tasks: tasks
+  });
+};
 
 export const detail = async (req: Request, res: Response) => {
   const id: string = req.params.id;
 
   const task = await Task.findOne({
     _id: id,
-    deleted: false
+    deleted: false,
   });
 
   res.json(task);
-}
+};
