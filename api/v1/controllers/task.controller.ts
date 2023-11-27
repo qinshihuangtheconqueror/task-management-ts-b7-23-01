c34cb6587e33fa112ba6255327ab3399509bcca0
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-11-27 22:56:34 +0700

    8. Xóa nhiều công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -96,12 +96,17 @@ export const changeStatus = async (req: Request, res: Response) => {
 // [PATCH] /api/v1/tasks/change-multi
 export const changeMulti = async (req: Request, res: Response) => {
   try {
+    enum Key {
+      STATUS = "status",
+      DELETE = "delete"
+    }
+
     const ids: string[] = req.body.ids;
     const key: string = req.body.key;
     const value: string = req.body.value;
 
     switch (key) {
-      case "status":
+      case Key.STATUS:
         await Task.updateMany(
           {
             _id: { $in: ids },
@@ -114,6 +119,23 @@ export const changeMulti = async (req: Request, res: Response) => {
         res.json({
           code: 200,
           message: "Cập nhật trạng thái thành công!",
+        });
+        break;
+
+      case Key.DELETE:
+        await Task.updateMany(
+          {
+            _id: { $in: ids }
+          },
+          {
+            deleted: true,
+            deletedAt: new Date()
+          }
+        );
+
+        res.json({
+          code: 200,
+          message: "Xóa thành công!"
         });
         break;
 
