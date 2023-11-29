c96f27b73501c4a8dc510579fd2b2a95d5bd020f
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-11-29 21:25:00 +0700

    13. Trang thông tin cá nhân

--- api/v1/controllers/user.controller.ts
+++ api/v1/controllers/user.controller.ts
@@ -71,3 +71,21 @@ export const login = async (req: Request, res: Response) => {
     token: token
   });
 };
+
+// [POST] /api/v1/users/detail/:id
+export const detail = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+
+  const user = await User.findOne({
+    _id: id,
+    deleted: false
+  }).select("-password -token");
+
+  console.log(user);
+
+  res.json({
+    code: 200,
+    message: "Thành công!",
+    info: user
+  });
+};
