e0098b82577a12915ac12a4d48e67e8d05419743
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2023-11-29 21:42:02 +0700

    14. Chuyển các route thành private

--- api/v1/controllers/user.controller.ts
+++ api/v1/controllers/user.controller.ts
@@ -72,20 +72,11 @@ export const login = async (req: Request, res: Response) => {
   });
 };
 
-// [POST] /api/v1/users/detail/:id
+// [POST] /api/v1/users/detail
 export const detail = async (req: Request, res: Response) => {
-  const id: string = req.params.id;
-
-  const user = await User.findOne({
-    _id: id,
-    deleted: false
-  }).select("-password -token");
-
-  console.log(user);
-
   res.json({
     code: 200,
     message: "Thành công!",
-    info: user
+    info: req["infoUser"]
   });
 };
